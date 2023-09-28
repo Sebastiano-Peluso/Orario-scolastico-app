618e39b18f27105d5eb97ba1442250cb15814909
SebastianoPeluso <sebastiano.peluso@istitutoagnelli.it>
2023-09-28 16:26:01 +0200

    add all

---
+++ docs/src/script.js
@@ -0,0 +1,30 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "220px";
+    
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
+
+  function color() {
+    document.getElementsByTagName("BODY")[0].style.backgroundColor = "#c7f9cc";
+    document.getElementsByClassName("sidebar")[0].style.backgroundColor = "#2c6e49";
+    document.getElementsByClassName("openbtn")[0].style.backgroundColor = "#2c6e49";
+    document.getElementsByClassName("change")[0].style.backgroundColor = "#007ea7";
+    
+    
+  }
+
+
+
+  
+
+
+
+
+
+
+
+  
